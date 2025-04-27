660319268e55aee12104f86ebbf54c6e2ac4c142
Hamza <mhamzatahir.ht@gmail.com>
2025-04-28 01:15:33 +0500

    Update shell.c

--- shell.c
+++ shell.c
@@ -3,57 +3,82 @@
 #include <string.h>
 #include <stdlib.h>
 #include <sys/wait.h>
-#define BUFFER_SIZE 256 
-#define ARGS_LIMIT 20 //Arguements limit
+#define BUFFER_SIZE 256
+#define ARGS_LIMIT 20  // Arguements limit
+#define HIST_LIMIT 500 // History limit
 
 void shell() // Function that runs the shell
 {
     char buffer[BUFFER_SIZE]; // Create a buffer to hold the input command
+    char *history[HIST_LIMIT];
+    int hist_counter = 0;
     while (1) // Infinite loop to keep the shell running
     {
         printf("mysh> "); // Display the prompt
-        if(fgets(buffer, BUFFER_SIZE, stdin) == NULL)
+        if (fgets(buffer, BUFFER_SIZE, stdin) == NULL)
         {
             printf("Exiting...\n");
             break;
         }
-        buffer[strlen(buffer)-1] = '\0'; // Remove the newline character from the input    
-        
-        if (strcmp(buffer, "exit") == 0) 
-        {
-            printf("Exiting...\n");
-            break;
-        }
-        
-        if (strlen(buffer) == 0) 
+        buffer[strlen(buffer) - 1] = '\0'; // Remove the newline character from the input
+
+        if (strlen(buffer) == 0)
         {
             printf("Nothing entered.\n");
             continue;
         }
 
-        char *args[ARGS_LIMIT];
-        int i=0;
-        char *token=strtok(buffer, " ");
-        // tokenize inputs into commands and arguements
-        while(token != NULL && i<ARGS_LIMIT-1)
+        if (hist_counter < HIST_LIMIT) // Adding the buffer string in the history string
         {
-            args[i]=token;
-            token=strtok(NULL, " ");
+            history[hist_counter] = strdup(buffer);
+            hist_counter++;
+        }
+        else
+        {
+            for (int i = 0; i < HIST_LIMIT - 1; i++)
+            {
+                history[i] = history[i + 1];
+            }
+            history[HIST_LIMIT - 1] = strdup(buffer);
+        }
+
+        if (strcmp(buffer, "exit") == 0)
+        {
+            printf("Exiting...\n");
+            break;
+        }
+
+        if (strcmp(buffer, "history") == 0)
+        {
+            for (int i = 0; i < hist_counter; i++)
+            {
+                printf("%d:  %s\n", i + 1, history[i]);
+            }
+            continue;
+        }
+        char *args[ARGS_LIMIT];
+        int i = 0;
+        char *token = strtok(buffer, " ");
+        // tokenize inputs into commands and arguements
+        while (token != NULL && i < ARGS_LIMIT - 1)
+        {
+            args[i] = token;
+            token = strtok(NULL, " ");
             i++;
         }
-        args[i]=NULL;
+        args[i] = NULL;
 
         pid_t pid = fork(); // Create a child process using fork()
-        if (pid < 0) // If fork fails
+        if (pid < 0)        // If fork fails
         {
             fprintf(stderr, "Fork failed\n");
             continue; // Continue the loop if fork fails
         }
         else if (pid == 0) // If we are in the child process
         {
-            execvp(args[0], args); // Execute the command entered by the user
+            execvp(args[0], args);                                    // Execute the command entered by the user
             fprintf(stderr, "Command '%s' not available.\n", buffer); // If execlp fails, print an error
-            exit(EXIT_FAILURE); // Exit the child process with failure status
+            exit(EXIT_FAILURE);                                       // Exit the child process with failure status
         }
         else // Parent process
         {
@@ -68,4 +93,3 @@ int main()
     shell(); // Call the shell function to start the shell
     return 0;
 }
-
