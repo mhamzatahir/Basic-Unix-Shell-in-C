e7a7623e5435384877efcb1699d040b9762d6390
Hamza <mhamzatahir.ht@gmail.com>
2025-04-28 14:18:27 +0500

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <signal.h>
 #include <stdlib.h>
 #include <sys/wait.h>
 #define BUFFER_SIZE 256
 #define ARGS_LIMIT 20  // Arguements limit
 #define HIST_LIMIT 500 // History limit
 
+void sigint_handler(int sig)
+{
+    // Do nothing to prevent exiting on Ctrl+C
+}
+
 void shell() // Function that runs the shell
 {
+    signal(SIGINT, sigint_handler); // Signal handler to ignore Ctrl+C
+
     char buffer[BUFFER_SIZE]; // Create a buffer to hold the input command
     char *history[HIST_LIMIT];
     int hist_counter = 0;
@@ -58,10 +66,26 @@ void shell() // Function that runs the shell
             continue;
         }
 
+        if (strncmp(buffer, "cd", 2) == 0) // Adding the cd command handling
+        {
+            char *dir = strtok(buffer + 3, " ");
+
+            if (dir == NULL) // If the user only types cd, go to HOME directory
+            {
+                dir = getenv("HOME");
+            }
+
+            if (chdir(dir) != 0) // Changing the directory
+            {
+                perror("cd"); // Print error if fails
+            }
+            continue;
+        }
+
         char *args[ARGS_LIMIT];
         int i = 0;
         char *token = strtok(buffer, " ");
-        
+
         while (token != NULL && i < ARGS_LIMIT - 1) // tokenize inputs into commands and arguements
         {
             args[i] = token;
@@ -72,16 +96,16 @@ void shell() // Function that runs the shell
 
         pid_t pid = fork(); // Create a child process using fork()
 
-        if (pid < 0)        // If fork fails
+        if (pid < 0) // If fork fails
         {
             fprintf(stderr, "Fork failed\n");
             continue; // Continue the loop if fork fails
         }
         else if (pid == 0) // If we are in the child process
         {
-            execvp(args[0], args);                                    // Execute the command entered by the user
-            fprintf(stderr, "Command '%s' not available.\n", buffer); // If execlp fails, print an error
-            exit(EXIT_FAILURE);                                       // Exit the child process with failure status
+            execvp(args[0], args);                                     // Execute the command entered by the user
+            fprintf(stderr, "Command '%s' not available.\n", args[0]); // If execlp fails, print an error
+            exit(EXIT_FAILURE);                                        // Exit the child process with failure status
         }
         else // Parent process
         {
@@ -90,7 +114,7 @@ void shell() // Function that runs the shell
         }
     }
 
-    for(int i=0; i<hist_counter; i++)
+    for (int i = 0; i < hist_counter; i++)
     {
         free(history[i]);
     }
